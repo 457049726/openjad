0f87c91d07b88d7cf8e5ede0f3e28a8781301901
hechuan <457049726@qq.com>
2021-06-11 10:09:03 +0800

    积累的更新

---
+++ openjad-orm/openjad-orm-api/src/main/java/com/openjad/orm/vo/RemarksEO.java
@@ -0,0 +1,6 @@
+package com.openjad.orm.vo;
+
+public interface RemarksEO extends BaseEO{
+	String getRemarks();
+	void setRemarks(String remarks);
+}
---
+++ openjad-orm/openjad-orm-api/src/main/java/com/openjad/orm/vo/StatusEO.java
@@ -0,0 +1,6 @@
+package com.openjad.orm.vo;
+
+public interface StatusEO extends BaseEO {
+	String getStatus();
+	void setStatus(String status);
+}
